f242993c9f8ece9e3143640e5b2ad00cded22fd0
BinAbdillah <baddillrag@gmail.com>
2026-08-10 00:35:08 +0700

    Add databaseURL to firebaseConfig object

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,6 +7,7 @@
    const firebaseConfig = {
     apiKey: "AIzaSyBVkq7jHNJsJWDYCTJ5JAYV3DiVAt62Vhc",
     authDomain: "mentas-013.firebaseapp.com",
+   databaseURL: "https://mentas-013-default-rtdb.asia-southeast1.firebasedatabase.app/",
     projectId: "mentas-013",
     storageBucket: "mentas-013.firebasestorage.app",
     messagingSenderId: "547287084550",
